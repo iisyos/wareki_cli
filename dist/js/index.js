adf0eb7a43bd9d652bd6c33cab526e7d2452599d
iisyos <kosuke1216a@yahoo.co.jp>
2023-01-04 20:15:43 +0900

    メインの機能作成

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,5 +1,16 @@
 #! /usr/bin/env node
-var hello = function (str) {
-    console.log(str);
+var today = function () {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var date = today.getDate();
+    return "".concat(year, "-").concat(month, "-").concat(date);
 };
-hello('hello world222111');
+(function () {
+    var _a;
+    var args = process.argv.slice(2);
+    var timeFormat = { era: "long" };
+    var input = (_a = args[0]) !== null && _a !== void 0 ? _a : today();
+    var date = new Date(input);
+    console.log(new Intl.DateTimeFormat("ja-JP-u-ca-japanese", timeFormat).format(date));
+})();
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,7 +1,19 @@
 #! /usr/bin/env node
+const today = (): string => {
+  let today = new Date();
 
-const hello = (str:string):void=>{
-    console.log(str);
-}
-hello('hello world222111')
+  let year = today.getFullYear();
+  let month = today.getMonth() + 1;
+  let date = today.getDate();
+  return `${year}-${month}-${date}`;
+};
 
+(() => {
+  const args = process.argv.slice(2);
+  const timeFormat: Intl.DateTimeFormatOptions = { era: "long" };
+  const input = args[0] ?? today();
+  const date = new Date(input);
+  console.log(
+    new Intl.DateTimeFormat("ja-JP-u-ca-japanese", timeFormat).format(date)
+  );
+})();
