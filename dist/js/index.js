1f48685b1ba6ef5fa9062a130f2a4ebb9c5a52f4
iisyos <kosuke1216a@yahoo.co.jp>
2023-01-04 17:29:10 +0900

    test

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,14 +1,7 @@
 #! /usr/bin/env node
 
 
-// var hello = function (str) {
-//     console.log(str);
-// };
-// hello('hello world222');
-
-
-function main() {
-    console.log("Hello ushio!");
-}
-
-main();
+var hello = function (str) {
+    console.log(str);
+};
+hello('hello world222');
