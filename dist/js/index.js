24e22f35d09ee7559ef768038ecd141684080a85
iisyos <kosuke1216a@yahoo.co.jp>
2023-01-04 17:31:27 +0900

    test

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,5 @@
 #! /usr/bin/env node
-
-
 var hello = function (str) {
     console.log(str);
 };
-hello('hello world222');
+hello('hello world222111');
--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,5 +1,7 @@
+#! /usr/bin/env node
+
 const hello = (str:string):void=>{
     console.log(str);
 }
-hello('hello world222')
+hello('hello world222111')
 
